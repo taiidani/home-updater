01c17b7fe9af5792a99a1a067d258826390d6dbb
Ryan Nixon <rnixon@pop-os.localdomain>
2020-09-09 23:51:37 -0700

    Convert job to single-run

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 )
 
 var domains = []string{
@@ -17,42 +16,37 @@ func main() {
 
 	fmt.Println("Application Starting")
 
-	for {
-		// Get the current external IP
-		fmt.Println("Extracting external IP address")
+	// Get the current external IP
+	fmt.Println("Extracting external IP address")
 
-		getter := IPGetter{}
-		ip, err := getter.Get(ctx)
+	getter := IPGetter{}
+	ip, err := getter.Get(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("IP discovered as " + ip)
+
+	// Update DNS to point at it
+	fmt.Println("Comparing IP against DigitalOcean records...")
+	updater := NewDigitalOceanUpdater()
+
+	for _, domain := range domains {
+		remoteIP, err := updater.GetCurrent(ctx, domain)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error extracting domain for %s: %s", domain, err)
 		}
+		fmt.Printf("DigitalOcean IP for %s is %s\n", domain, remoteIP)
 
-		fmt.Println("IP discovered as " + ip)
-
-		// Update DNS to point at it
-		fmt.Println("Comparing IP against DigitalOcean records...")
-		updater := NewDigitalOceanUpdater()
-
-		for _, domain := range domains {
-			remoteIP, err := updater.GetCurrent(ctx, domain)
+		if remoteIP == ip {
+			fmt.Println("IP Addresses match. Skipping update of DNS record")
+		} else {
+			err = updater.Update(ctx, domain, ip)
 			if err != nil {
-				log.Fatalf("Error extracting domain for %s: %s", domain, err)
+				log.Fatal(err)
 			}
-			fmt.Printf("DigitalOcean IP for %s is %s\n", domain, remoteIP)
 
-			if remoteIP == ip {
-				fmt.Println("IP Addresses match. Skipping update of DNS record")
-			} else {
-				err = updater.Update(ctx, domain, ip)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				fmt.Printf("Done! New IP address for %s set to %s\n", domain, ip)
-			}
+			fmt.Printf("Done! New IP address for %s set to %s\n", domain, ip)
 		}
-
-		fmt.Println("Sleeping for 120 minutes")
-		time.Sleep(time.Minute * 120)
 	}
 }
